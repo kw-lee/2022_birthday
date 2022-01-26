f06233d8ccb58708fccf4e7245097ed726c75fc2
Kyeongwon Lee <lkw1718@snu.ac.kr>
2022-01-26 09:20:12 +0900

    implement smooth scrolling

--- js/nav.js
+++ js/nav.js
@@ -1,18 +1,25 @@
-// The function actually applying the offset
-function offsetAnchor() {
-  if (location.hash.length !== 0) {
-    window.scrollTo(window.scrollX, window.scrollY - 55);
-  }
-}
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
 
-// Captures click events of all <a> elements with href starting with #
-$(document).on('click', 'a[href^="#"]', function(event) {
-  // Click events are captured before hashchanges. Timeout
-  // causes offsetAnchor to be called after the page jump.
-  window.setTimeout(function() {
-    offsetAnchor();
-  }, 0);
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 600, function(){
+   
+      // Add hash (#) to URL when done scrolling (default click behavior)
+      // window.location.hash = hash;
+      history.pushState({}, "", hash);
+      });
+    } // End if
+  });
 });
-
-// Set the offset when entering page with hash present in the url
-window.setTimeout(offsetAnchor, 0);
