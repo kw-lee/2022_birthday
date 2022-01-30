8eb61ef1b0d1180dd90ad3f2a15a4641b0bc9af0
Kyeongwon Lee <lkw1718@snu.ac.kr>
2022-01-30 21:58:35 +0900

    end project

--- js/dday.js
+++ js/dday.js
@@ -7,6 +7,9 @@ setInterval(function() {
   var hour = Math.floor((gap % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var min = Math.floor((gap % (1000 * 60 * 60)) / (1000 * 60));
   var sec = Math.floor((gap % (1000 * 60)) / 1000);
-
-  document.getElementById("dday").innerHTML = "프로젝트 종료까지 " + day + "일 " + hour + "시간 " + min + "분 " + sec + "초 남았습니다.";
+  if (day >= 0) {
+    document.getElementById("dday").innerHTML = "프로젝트 종료까지 " + day + "일 " + hour + "시간 " + min + "분 " + sec + "초 남았습니다.";
+  } else {
+    document.getElementById("dday").innerHTML = "프로젝트가 종료되었습니다.";
+  }
 }, 1000);
